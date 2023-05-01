9c1365e7771f2f6fa24fc9789c22a93be5b859c3
TomasBarnas93 <btomek506@gmail.com>
2023-05-01 11:46:20 +0200

    Fix

--- Helpers/Repositories/BaseRepository.cs
+++ Helpers/Repositories/BaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Manero_Backend.Contexts;
 using Manero_Backend.Models.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
 using System.Linq.Expressions;
 
 namespace Manero_Backend.Repositories
@@ -18,59 +19,89 @@ namespace Manero_Backend.Repositories
 
         #region CRUD
 
-        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        public virtual async Task<IEnumerable<TEntity>> GetAllAsync()
         {
-            return await _context.Set<TEntity>().ToListAsync();
+			try
+			{
+				var result = await _context.Set<TEntity>().ToListAsync();
+				return result;
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+			return null!;
 
-        }
-        public async Task<TEntity> GetByIdAsync(Guid id)
+		}
+        public virtual async Task<TEntity> GetByIdAsync(Guid id)
         {
             var result = await _context.FindAsync<TEntity>(id);
 
             return result!;
 
         }
-        public async Task<TEntity> CreateAsync(TEntity entity)
+        public virtual async Task<TEntity> CreateAsync(TEntity entity)
         {
-            _context.Set<TEntity>().Add(entity);
-            await _context.SaveChangesAsync();
-            return entity;
+			try
+			{
+				_context.Set<TEntity>().Add(entity);
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+			return entity;
+		}
+
+        public virtual async Task RemoveAsync(TEntity entity)
+        {
+            try
+            {
+				_context?.Set<TEntity>().Remove(entity);
+				await _context!.SaveChangesAsync();
+			}
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }  
         }
 
-        public async Task RemoveAsync(TEntity entity)
+        public virtual async Task<TEntity> UpdateAsync(TEntity entity)
         {
-            _context?.Set<TEntity>().Remove(entity);
-            await _context!.SaveChangesAsync();
-        }
-
-        public async Task<TEntity> UpdateAsync(TEntity entity)
-        {
-            _context.Set<TEntity>().Update(entity);
-            await _context!.SaveChangesAsync();
-            return entity;
-        }
+			try
+			{
+				_context.Set<TEntity>().Update(entity);
+				await _context.SaveChangesAsync();
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex.Message);
+			}
+			return entity;
+		}
         #endregion
 
-        public async Task<IEnumerable<TEntity>> SearchAsync(Expression<Func<TEntity, bool>> predicate)
+        public virtual async Task<IEnumerable<TEntity>> SearchAsync(Expression<Func<TEntity, bool>> predicate)
         {
-            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+            try
+            {
+				return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+			}
+			catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex.Message);
+            }
+            return null!;
         }
 
-        public async Task<IEnumerable<TEntity>> GetAllByTag(string tag)
+        public virtual async Task<IEnumerable<TEntity>> GetAllBy(string byTerm)
         {
-            var result = await _context.FindAsync<IEnumerable<TEntity>>(tag);
+            var result = await _context.FindAsync<IEnumerable<TEntity>>(byTerm);
 
             return result!;
 
         }
-        public async Task<IEnumerable<TEntity>> GetAllByGenre(string genre)
-        {
-            var result = await _context.FindAsync<IEnumerable<TEntity>>(genre);
-
-            return result!;
-
-        }
-
-
     }
 }
